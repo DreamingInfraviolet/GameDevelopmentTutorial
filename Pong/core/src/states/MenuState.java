bdcab1f1248f056eae733cb567f859502e973d2a
Mark Kearns <markdavidkearns@outlook.com>
2015-10-19 23:16:36 +0100

    Revert "Removed old version (broken import) and replaced with newer version"
    This reverts commit 379103ea126d8410bffbd165350a62230efe52b7.

---
+++ Pong/core/src/states/MenuState.java
@@ -0,0 +1,51 @@
+package states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mugamedev.game.Pong;
+
+public class MenuState extends State
+{
+	private Texture background;
+	private Texture playbtn;
+	
+	public MenuState(GameStateManager gsm)
+	{
+		super(gsm);
+		background = new Texture("main.png");
+		playbtn = new Texture("startB.png");
+	}
+
+	@Override
+	public void handleInput(float dt) 
+	{
+		if(Gdx.input.justTouched())
+		{
+			gsm.push(new PlayState(gsm));
+			dispose();
+		}
+	}
+
+	@Override
+	public void update(float dt) 
+	{
+		handleInput(dt);
+	}
+
+	@Override
+	public void render(SpriteBatch sb) 
+	{
+		sb.begin();
+		sb.draw(background, 0, 0, Pong.WIDTH, Pong.HEIGHT);
+		sb.draw(playbtn, (Pong.WIDTH / 2) - (playbtn.getWidth() /2), (Pong.HEIGHT / 2) - (playbtn.getHeight() / 2));
+		sb.end();
+	}
+	
+	@Override
+	public void dispose() 
+	{
+		background.dispose();
+		playbtn.dispose();
+	}
+}
